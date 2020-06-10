295c612d410d4481fa59f92d6df26e07d52ee5f7
kanishk-anand-oyo <kanishk.anand@oyorooms.com>
2020-06-10 22:41:12 +0530

    Server logic added + CSS changes + API integration

--- server/index.js
+++ server/index.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const port = 8000;
+const messages = require("./message.json");
+const members = require("./members.json");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve("../client/build/index.html"))
+);
+
+app.get("/static/:folder/:file", (req, res) => {
+  res.sendFile(
+    path.resolve(
+      `../client/build/static/${req.params.folder}/${req.params.file}`
+    )
+  );
+});
+
+app.get("/members", (req, res) => {
+  res.status(200).json({ data: members });
+});
+
+app.get("/message", (req, res) => {
+  res.status(200).json({ data: messages });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
